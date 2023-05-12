96f08c9a81604fc30170ac69b65eea6fbcdaf594
yunho1263 <31506451+yunho1263@users.noreply.github.com>
2023-05-12 12:15:29 +0900

    textMeshPro 폰트 추가

---
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+	enum InputModeState
+	{
+        CharacterControl = 0,
+        MagicSpellInput,
+
+        None
+    }
+
+
+}
